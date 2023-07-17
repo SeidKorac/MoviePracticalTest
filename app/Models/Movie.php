4b1acde45808ec3b73f993ed4f6d23d2da56e81a
Seid Korac <seidkorac@Seids-MacBook-Air.local>
2023-07-17 04:50:57 +0200

    added slugs to movies

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Movie extends Model
 {
-    use HasFactory;
+    use HasFactory, hasSlug;
 
     protected $fillable = [
         'title',
@@ -17,6 +19,27 @@ class Movie extends Model
         'duration',
         'releaseDate',
     ];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug')
+            ->usingSeparator('_');
+    }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 
     public function genre()
     {
